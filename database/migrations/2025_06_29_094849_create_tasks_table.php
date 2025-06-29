a10e8b1fcd8a2ffed6a0ed00370d83fea0eb90b8
Abderrahmane_Ahouari <abdorrahmane054@gmail.com>
2025-06-29 11:00:09 +0100

    created the migration for task now going for project

--- database/migrations/2025_06_29_094849_create_tasks_table.php
+++ database/migrations/2025_06_29_094849_create_tasks_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status', ['pending', 'in_progress', 'completed'])->default('pending');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('project_id')->nullable();
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+            $table->dateTime('due_date')->nullable();
+            $table->integer('priority')->default(0); // 0: low, 1: medium, 2: high 
             $table->timestamps();
         });
     }
