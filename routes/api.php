b53b6aa4473433848195212196921b5da38000df
Abderrahmane_Ahouari <abdorrahmane054@gmail.com>
2025-07-02 15:14:23 +0100

    added some routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Taskcontroller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,7 +19,15 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    // Task routes
+    Route::post('/tasks', [Taskcontroller::class, 'store']);
+    Route::put('/tasks/{id}', [Taskcontroller::class, 'update']);
+    Route::delete('/tasks/{id}', [Taskcontroller::class, 'destroy']);
+    Route::get('/projects/{project_id}/tasks', [Taskcontroller::class, 'indexByProject']);
 });
 
